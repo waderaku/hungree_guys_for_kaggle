870619cbe09c0c2ba319c46cacee4057e602563e
yotaro-shimose <yosemat.beta@gmail.com>
2021-05-23 08:14:44 +0000

    to_tf_tensorを採用

--- geese/util/converter.py
+++ geese/util/converter.py
@@ -5,7 +5,7 @@ from geese.structure import Observation
 from geese.constants import FIELD_HEIGHT, FIELD_WIDTH, NUM_CHANNELS
 
 
-def to_tf_tensor(self, obs: Observation, last_obs: Observation, index: int) -> tf.Tensor:
+def to_tf_tensor(obs: Observation, last_obs: Observation, index: int) -> tf.Tensor:
     b = np.zeros((NUM_CHANNELS, FIELD_HEIGHT * FIELD_WIDTH), dtype=np.float32)
 
     for p, pos_list in enumerate(obs.geese):
@@ -29,4 +29,4 @@ def to_tf_tensor(self, obs: Observation, last_obs: Observation, index: int) -> t
     for pos in obs.food:
         b[16, pos] = 1
 
-    return b.reshape(-1, 7, 11)
+    return tf.convert_to_tensor(b.reshape(-1, FIELD_HEIGHT, FIELD_WIDTH))
