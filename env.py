28deee4b19ae6c63d26dae5314a679e44d3826f6
Administrator <satoshi.hieda0308@gmail.com>
2021-05-23 14:22:25 +0900

    Initial Commit

--- env.py
+++ env.py
@@ -1,12 +1,26 @@
 from kaggle_environments import make, evaluate
+from kaggle_environments.envs.hungry_geese.hungry_geese import Action, translate
+from typing import List
 
 
 class Env():
     def __init__(
-        num_rows=7,
-        num_colums=11,
-        action_list=[]
-    ):
+        num_rows: int = 7,
+        num_colums: int = 11,
+        action_list: List[Action] = [Action.NORTH,
+                                     Action.SOUTH, Action.WEST, Action.EAST],
+        hunger_rate: int = 40
+    ) -> None:
+        raise NotImplementedError()
+
+    def getActionSize(self):
+        return len(self.actions)
+
+    def step(self,
+             obs: Observation,
+             last_obs: Observation,
+             direction_list: List[int]) -> None:
+        return
 
 
 env = make("hungry_geese", debug=False)
