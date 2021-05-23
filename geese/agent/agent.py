e8dad05861111ac3eddef23751fafeb1294ade2e
Administrator <satoshi.hieda0308@gmail.com>
2021-05-23 17:47:28 +0900

    エージェントの機能作成

--- geese/agent/agent.py
+++ geese/agent/agent.py
@@ -1,18 +1,49 @@
+from kaggle_environments.envs.hungry_geese.hungry_geese import Action
+from geese.agent.mcts import MCTS
+
 import tensorflow as tf
+import numpy as np
+
+from geese.constants import ACTIONLIST
 
 
 class Agent():
     def __init__(
         self,
         model: tf.keras.models.Model,
+        eps: float = 1.0,
+        eps_anneal: float = 0.9995,
+        eps_min: float = 0.15,
     ):
         self.model = model
+        self.eps = eps
+        self.eps_anneal = eps_anneal
+        self.eps_min = eps_min
 
-    def get_action(self, tf_obs: tf.Tensor) -> tf.Tensor:
-        pass
+    def get_action(self, tf_obs: tf.Tensor) -> Action:
+        mcts = MCTS(self.model)
+
+        prob = mcts.get_prob()
+
+        # ε-greedyで次アクションをチョイス
+        prob = prob.numpy()
+        return self._eps_greedy(prob)
+
+    def _eps_greedy(self, prob: np.array) -> Action:
+        rand = np.random.rand()
+        self._update_eps()
+        if self.eps > rand:
+            next_action = np.random.choice(ACTIONLIST)
+        else:
+            next_action = np.random.choice(ACTIONLIST, p=prob)
+        self._update_eps()
+        return next_action
+
+    def _update_eps(self) -> None:
+        self.eps = np.max([self.eps*self.eps_anneal, self.eps_min])
 
     def save(self, path: str) -> None:
-        raise NotImplementedError
+        self.model.save(path)
 
     def load(self, path: str) -> None:
-        raise NotImplementedError
+        self.model = tf.keras.models.load_model(path)
--- geese/constants.py
+++ geese/constants.py
@@ -1,5 +1,10 @@
+from kaggle_environments.envs.hungry_geese.hungry_geese import Action
 # Field Size
 FIELD_HEIGHT = 7
 FIELD_WIDTH = 11
 
 NUM_CHANNELS = 17
+
+# ActionList
+ACTIONLIST = [Action.NORTH,
+              Action.SOUTH, Action.WEST, Action.EAST]
