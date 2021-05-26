fbfac1c11e91ea85db4e75bb0d32e012957fe5fe
yotaro-shimose <yosemat.beta@gmail.com>
2021-05-26 20:50:12 +0900

    Envをラップしました

--- geese/structure/observation.py
+++ geese/structure/observation.py
@@ -1,20 +1,5 @@
-from kaggle_environments.envs.hungry_geese.hungry_geese import Observation as Obs
+from typing import TypeVar
 
+import numpy as np
 
-class Observation:
-    def __init__(self, now_obs: Obs, last_obs: Obs, index: int):
-        self._now_obs = now_obs
-        self._last_obs = last_obs
-        self._index = index
-
-    @property
-    def now_obs(self):
-        return self._now_obs
-
-    @property
-    def last_obs(self):
-        return self._last_obs
-
-    @property
-    def index(self):
-        return self._index
+Observation = TypeVar("Observation", bound=np.ndarray)
