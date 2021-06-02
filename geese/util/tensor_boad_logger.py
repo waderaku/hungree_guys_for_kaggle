2d1b051047574c2e03388cdf61212431247e4f87
waderaku <wadebosh@icloud.com>
2021-06-02 10:55:21 +0900

    logのstepの持ち方を変更

--- geese/util/tensor_boad_logger.py
+++ geese/util/tensor_boad_logger.py
@@ -6,9 +6,10 @@ import tensorflow as tf
 class TensorBoardLogger():
     def __init__(self, log_dir: str):
         self._logger = tf.summary.create_file_writer(log_dir)
-        self.step = 1
+        self._step_dict = {}
 
     def logging_scaler(self, data_label: str, data: Any) -> None:
+        step = self._step_dict.get(data_label, 1)
         with self._logger.as_default():
-            tf.summary.scalar(data_label, data, self.step)
-        self.step += 1
+            tf.summary.scalar(data_label, data, step)
+        self._step_dict[data_label] = step+1
